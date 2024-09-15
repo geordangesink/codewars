3afda0e0709000ef7f88d7d4d96a83e2cfd5c61e
geordangesink <georgegeordan@gmail.com>
2024-09-15 22:18:41 +0200

    15.09.2024

---
+++ 6-is-a-prime-number.js
@@ -0,0 +1,26 @@
+/*
+JavaScript
+
+Description:
+Define a function that takes an integer argument and returns a logical value true or false depending on if the integer is a prime.
+
+Per Wikipedia, a prime number ( or a prime ) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+Requirements
+You can assume you will be given an integer input.
+You can not assume that the integer will be only positive. You may be given negative numbers as well ( or 0 ).
+NOTE on performance: There are no fancy optimizations required, but still the most trivial solutions might time out. Numbers go up to 2^31 ( or similar, depending on language ). Looping all the way up to n, or n/2, will be too slow.
+*/
+
+function isPrime(num) {
+  if (num < 2) return false; // check for 2
+
+  // check every number until squareroot of num since that is where the factor or quotient is always bigger than either the factor or quorient of any other posibility
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
