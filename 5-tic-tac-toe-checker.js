5248820af53222bc6c246363c6ec35f557a6593a
Geordan Gesink <georgegeordan@gmail.com>
2025-02-14 16:02:48 +0100

    14.02.2025

--- 5-tic-tac-toe-checker.js
+++ 5-tic-tac-toe-checker.js
@@ -18,27 +18,82 @@ You may assume that the board passed in is valid in the context of a game of Tic
 */
 
 function isSolved(board) {
-    // loop for player 1 and player 2
-    for ( num = 1; num <= 2; num++ ){
-      for ( i = 0; i < 3; i++){
-        // check for horizontal lines
-        if ( (board[i][0] == num && board[i][1] == num && board[i][2] == num) ||
+  // loop for player 1 and player 2
+  for (num = 1; num <= 2; num++) {
+    for (i = 0; i < 3; i++) {
+      // check for horizontal lines
+      if (
+        (board[i][0] == num && board[i][1] == num && board[i][2] == num) ||
         // check for vertical lines
-           (board[0][i] == num && board[1][i] == num && board[2][i] == num) ||
+        (board[0][i] == num && board[1][i] == num && board[2][i] == num) ||
         // check for diogonal (top left to bottom right)
-           (board[0][0] == num && board[1][1] == num && board[2][2] == num) ||
+        (board[0][0] == num && board[1][1] == num && board[2][2] == num) ||
         // check for diogonal (top right to bottom left)
-           (board[0][2] == num && board[1][1] == num && board[2][0] == num)){
-          return num;
-        }
+        (board[0][2] == num && board[1][1] == num && board[2][0] == num)
+      ) {
+        return num;
       }
     }
-    // if game is not done yet
-    if ( board[0].includes(0) || board[1].includes(0) || board[2].includes(0) ){
-      return -1;
-    }
-    // if its a draw
-    else{
-      return 0;
-    }
   }
+  // if game is not done yet
+  if (board[0].includes(0) || board[1].includes(0) || board[2].includes(0)) {
+    return -1;
+  }
+  // if its a draw
+  else {
+    return 0;
+  }
+}
+
+// alternative:
+function isSolved(board) {
+  const field = {
+    empty: 0,
+    x: 1,
+    o: 2,
+  };
+
+  const notFinished = -1;
+  const draw = 0;
+
+  return (
+    checkDiagonal() || checkVertical() || checkHorizontal() || (isDraw() === 0 ? 0 : notFinished)
+  );
+
+  function checkDiagonal() {
+    // check if middle field is empty
+    if (board[1][1] !== field.empty) {
+      // check of diagonal
+      if (
+        (board[1][1] === board[0][0] && board[1][1] === board[2][2]) ||
+        (board[1][1] === board[0][2] && board[1][1] === board[2][0])
+      ) {
+        return board[1][1];
+      }
+    }
+    return undefined;
+  }
+
+  function checkVertical() {
+    for (var i = 0; i < 3; i++) {
+      // check for vertical in each column
+      if (board[0][i] === board[1][i] && board[0][i] === board[2][i]) {
+        return board[0][i];
+      }
+    }
+    return undefined;
+  }
+
+  function checkHorizontal() {
+    for (var row of board) {
+      if (row.every((value) => value === row[0])) {
+        return row[0];
+      }
+    }
+    return undefined;
+  }
+
+  function isDraw() {
+    return board.flat().includes(field.empty) ? undefined : draw;
+  }
+}
